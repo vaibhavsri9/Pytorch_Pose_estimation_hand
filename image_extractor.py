10875d4138687179ae2bdbd55b5f736d70773211
vaibhav <srivastava.va@husky.neu.edu>
2020-03-09 15:51:33 -0400

    first commit

---
+++ image_extractor.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+Extract Images from a topic
+"""
+
+from __future__ import print_function
+
+
+import os
+import argparse
+import sys
+
+import cv2
+"""
+sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages') # append back in order to import rospy
+"""
+
+import roslib
+import rospy
+import rosbag
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+
+def main():
+
+    """
+    Extract a folder of images from a bag file
+    """
+    parser = argparse.ArgumentParser(description="Extract images from a ROS bag.")
+    parser.add_argument("--bag_file",required="true", help="Input ROS bag.")
+    parser.add_argument("--output_dir", help="Output directory.", default="/home/vaibhav/test")
+    parser.add_argument("--image_topic", help="Image topic.", default="/camera/rgb/image_rect_color/compressed")
+
+    args = parser.parse_args()
+
+    print(args.bag_file,args.image_topic)
+
+    bag = rosbag.Bag(args.bag_file, "r")
+    bridge = CvBridge()
+    count = 0
+
+    for topic, msg, t in bag.read_messages(topics=[args.image_topic]):
+        cv_img = bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="passthrough")
+        #cv2.imshow('mat',cv_img)
+	#cv2.waitKey(3)
+	filename=args.output_dir + '/frame_{}.png'.format(count)
+	print(filename)
+        cv2.imwrite(filename, cv_img)
+        count += 1
+	print(count)
+
+    bag.close()
+
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ pose_extractor.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""
+Extract Pose for every message and take the average of every N poses and write it to a file. N is the ratio of image messages to Pose messages
+"""
+from __future__ import print_function
+import math
+import csv
+
+# OpenVR package
+import openvr_ros
+
+# ROS packages
+import roslib
+import rospy
+import rosbag
+import tf
+import argparse
+
+# ROS messages
+from geometry_msgs.msg import *
+from std_msgs.msg import *
+from openvr_ros.msg import TrackedDevicePose
+
+# Constants
+
+
+def main():
+	"""
+	Extract the csv file with pose annotations
+	"""
+	parser = argparse.ArgumentParser(description="Extract pose annotations from a ROS bag.")
+	parser.add_argument("--bag_file",required="true", help="Input ROS bag.")
+	args = parser.parse_args()
+	
+	print(args.bag_file)
+	bag = rosbag.Bag(args.bag_file, "r")
+	count = 0
+	rowx = []
+	rowy = []
+	rowz = []
+	rowox = []
+	rowoy = []
+	rowoz = []
+	rowow = []
+# Add angles and twist information as well	
+	for topic,msg,t in bag.read_messages(topics=['/track_publisher/tracked_device_pose']):		
+		rowx.append(msg.pose.position.x)		
+		rowy.append(msg.pose.position.y)
+		rowz.append(msg.pose.position.z)
+		rowox.append(msg.pose.orientation.x)
+		rowoy.append(msg.pose.orientation.y)
+		rowoz.append(msg.pose.orientation.z)		
+		rowow.append(msg.pose.orientation.w)
+		print(msg.pose)			
+	bag.close()
+	print(count)
+	with open('All_poses.csv', 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(rowx)
+		writer.writerow(rowy)
+		writer.writerow(rowz)
+		writer.writerow(rowox)
+		writer.writerow(rowoy)
+		writer.writerow(rowoz)
+		writer.writerow(rowow)
+
+#def Createsamples(input_file,N)  
+
+if __name__ == '__main__':
+    main()
