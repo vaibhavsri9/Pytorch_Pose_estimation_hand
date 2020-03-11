556269274c7e3659bcb4d44b9dd2b4722de993bc
vaibhav <srivastava.va@husky.neu.edu>
2020-03-10 22:10:42 -0400

    Code for Sampling pose included

--- pose_extractor.py
+++ pose_extractor.py
@@ -7,7 +7,7 @@ import math
 import csv
 
 # OpenVR package
-import openvr_ros
+#import openvr_ros
 
 # ROS packages
 import roslib
@@ -24,14 +24,14 @@ from openvr_ros.msg import TrackedDevicePose
 # Constants
 
 
-def pose_extractor():
+def main():
 	"""
 	Extract the csv file with pose annotations
 	"""
 	parser = argparse.ArgumentParser(description="Extract pose annotations from a ROS bag.")
 	parser.add_argument("--bag_file",required="true", help="Input ROS bag.")
 	args = parser.parse_args()
-	
+
 	print(args.bag_file)
 	bag = rosbag.Bag(args.bag_file, "r")
 	count = 0
@@ -42,29 +42,47 @@ def pose_extractor():
 	rowoy = []
 	rowoz = []
 	rowow = []
-# Add angles and twist information as well	
-	for topic,msg,t in bag.read_messages(topics=['/track_publisher/tracked_device_pose']):		
-		rowx.append(msg.pose.position.x)		
+# Add angles and twist information as well
+	for topic,msg,t in bag.read_messages(topics=['/track_publisher/tracked_device_pose']):
+		rowx.append(msg.pose.position.x)
 		rowy.append(msg.pose.position.y)
 		rowz.append(msg.pose.position.z)
 		rowox.append(msg.pose.orientation.x)
 		rowoy.append(msg.pose.orientation.y)
-		rowoz.append(msg.pose.orientation.z)		
+		rowoz.append(msg.pose.orientation.z)
 		rowow.append(msg.pose.orientation.w)
-		print(msg.pose)			
+		print(msg.pose)
 	bag.close()
-	return count
+	print(count)
+	rowxavg = sample_row(rowx)
+	rowyavg = sample_row(rowy)
+	rowzavg = sample_row(rowz)
+	rowoxavg = sample_row(rowox)	
+	rowoyavg = sample_row(rowoy)	
+	rowozavg = sample_row(rowoz)		
+	rowowavg = sample_row(rowow)
 	with open('All_poses.csv', 'wb') as f:
 		writer = csv.writer(f)
-		writer.writerow(rowx)
-		writer.writerow(rowy)
-		writer.writerow(rowz)
-		writer.writerow(rowox)
-		writer.writerow(rowoy)
-		writer.writerow(rowoz)
-		writer.writerow(rowow)
+		writer.writerow(rowxavg)
+		writer.writerow(rowyavg)
+		writer.writerow(rowzavg)
+		writer.writerow(rowoxavg)
+		writer.writerow(rowoyavg)
+		writer.writerow(rowozavg)
+		writer.writerow(rowowavg)
+	return
 
-#def Createsamples(input_file,N)  
+
+
+def sample_row(row):
+	rowavg = []
+	y = len(row)//18 + len(row)%18	
+	for i in range(0,y):	
+		smallrow = sum(row[i:i+18])/18
+		rowavg.append(smallrow)
+	print(len(rowavg))
+	return rowavg	
+
 
 if __name__ == '__main__':
-    pose_no = pose_extractor()
+    main()
