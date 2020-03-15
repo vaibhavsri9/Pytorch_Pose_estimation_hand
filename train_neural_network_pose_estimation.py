9a2af0da371793a316e3b5950cca97733d8d387a
vaibhav <srivastava.va@husky.neu.edu>
2020-03-15 17:30:34 -0400

    Modified file for training

--- train_neural_network_pose_estimation.py
+++ train_neural_network_pose_estimation.py
@@ -11,31 +11,29 @@ import torch.nn as nn
 import numpy as np
 
 
+
 transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-
-trainset = HandPoseDataset(root_dir = 'test/',csv_file='test/All_poses.csv,transform = transform)
-trainloader = torch.utils.data.DataLoader(trainset,batch_size=4,shuffle=True,num_workers=1)
+    [transforms.ToPILImage(),transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+trainset = HandPoseDataset(root_dir = 'test/',csv_file='test/All_poses.csv',transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,batch_size=4,shuffle=False,num_workers=1)
 net = Net()
-
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.1, momentum =0.9)
 
-for epoch in range(2):
 
-    running_loss = 0.0
-    for i,data in enumerate(trainloader, 0):
-        inputs, labels = data
+for epoch in range(10):
+    for i,data in enumerate(trainloader):
+        inputs = data['image']
+        labels = data['Pose']
         optimizer.zero_grad()
         outputs = net(inputs)
-        loss = criterion(ouputs, labels)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
         #print statistics
-        runnin_loss += loss.item()
+        running_loss += loss.item()
+        print(epoch+1,i + 1,running_loss)
 
 
 print('Finished Training')
